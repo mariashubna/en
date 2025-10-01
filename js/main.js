8d68ad966421bf2ceed761e810c1ed9f5e9d0808
Mariia <mariashubna@gmail.com>
2025-10-01 09:04:11 +0200

    copy

---
+++ js/main.js
@@ -0,0 +1,225 @@
+document.getElementById("form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const form = e.target;
+  const data = new FormData(form);
+  const action = form.action;
+
+  const xhr = new XMLHttpRequest();
+  xhr.open("POST", action, true);
+
+  xhr.onload = function () {
+    if (xhr.status === 200) {
+      try {
+        const result = JSON.parse(xhr.responseText);
+        if (result.result === "Success") {
+          const currentPath = window.location.pathname;
+          if (currentPath === "/" || currentPath === "/index.html") {
+            window.location.href = "https://prt.mn/TEY37tjI2";
+          } else if (currentPath === "/tai_chi.html") {
+            window.location.href = "https://prt.mn/DC82FfqAwSt";
+          }
+        } else {
+          alert("Error sending data.");
+        }
+      } catch (err) {
+        console.error("Unable to make out the answer:", err);
+      }
+    } else {
+      console.error("HTTP Error:", xhr.status);
+    }
+  };
+
+  xhr.onerror = function () {
+    console.error("Error while requesting");
+  };
+
+  xhr.send(data);
+});
+
+$(".buy_course_btn").on("click", function (e) {
+  e.preventDefault();
+
+  const target = $($(this).attr("href"));
+  if (target.length) {
+    const offset = $(window).height() / 2 - target.outerHeight() / 2;
+    const scrollToPosition = target.offset().top - offset;
+
+    $("html, body").animate(
+      {
+        scrollTop: scrollToPosition,
+      },
+      800
+    );
+  }
+});
+
+//show course block
+$("#btn_course").on("click", function () {
+  $(".drop_list").fadeToggle("fast");
+});
+
+$(window).on("pageshow", function (event) {
+  $(".drop_list").hide();
+});
+
+$("#offerta").on("click", function () {
+  $("#offerta_visib").fadeIn("fast");
+  $(".overlay").fadeIn("fast");
+});
+$(".overlay").on("click", function () {
+  $("#offerta_visib").fadeOut("fast");
+  $(".overlay").fadeOut("fast");
+});
+$("#close_modal_policy").on("click", function () {
+  $("#offerta_visib").fadeOut("fast");
+  $(".overlay").fadeOut("fast");
+});
+$("#close_modal_policy2").on("click", function () {
+  $("#offerta_visib").fadeOut("fast");
+  $(".overlay").fadeOut("fast");
+});
+
+$("#polyci").on("click", function () {
+  $("#visib_policy_two").fadeIn("fast");
+  $("#overlay_two").fadeIn("fast");
+});
+$("#overlay_two").on("click", function () {
+  $("#visib_policy_two").fadeOut("fast");
+  $("#overlay_two").fadeOut("fast");
+});
+$(".close_modal_policy").on("click", function () {
+  $("#visib_policy_two").fadeOut("fast");
+  $("#overlay_two").fadeOut("fast");
+});
+$(".close_modal_policy2").on("click", function () {
+  $("#visib_policy_two").fadeOut("fast");
+  $("#overlay_two").fadeOut("fast");
+});
+
+// Слайдер на відгуки
+
+const slider = document.querySelector(".wrapp_feedback_items");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return;
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const walk = (x - startX) * 2;
+  slider.scrollLeft = scrollLeft - walk;
+});
+
+// Заборона на копіювання тексту в відгуках
+slider.addEventListener("copy", (e) => {
+  e.preventDefault();
+});
+
+// Анімований список
+let consumerList = document.querySelector(".consumer_list");
+let blockYinTop = document.querySelectorAll(".block_yin_top");
+let blockYin = document.querySelectorAll(".second_list");
+let aboutBtn = document.querySelectorAll(".about_btn");
+let resultBtn = document.querySelectorAll(".result_btn");
+let feedbackList = document.querySelector(".wrapp_feedback_items");
+
+function isInViewport(element) {
+  if (!element) return false;
+  const rect = element.getBoundingClientRect();
+  const windowHeight =
+    window.innerHeight || document.documentElement.clientHeight;
+  const windowWidth = window.innerWidth || document.documentElement.clientWidth;
+
+  const visibleHeight =
+    Math.min(rect.bottom, windowHeight) - Math.max(rect.top, 0);
+
+  if (windowWidth >= 1280) {
+    return visibleHeight >= 100;
+  } else {
+    return visibleHeight >= 50;
+  }
+}
+
+function handleFadeIn(element, extraElements = []) {
+  if (isInViewport(element)) {
+    element.classList.add("anim-fade-in");
+    extraElements.forEach((el) => el.classList.add("btn-anim"));
+  }
+}
+
+if (consumerList) {
+  handleFadeIn(consumerList);
+  window.addEventListener("scroll", () => handleFadeIn(consumerList));
+}
+
+if (feedbackList) {
+  handleFadeIn(feedbackList);
+  window.addEventListener("scroll", () => handleFadeIn(feedbackList));
+}
+
+blockYin.forEach((blockYinItem) => {
+  handleFadeIn(blockYinItem, resultBtn);
+});
+window.addEventListener("scroll", () => {
+  blockYin.forEach((blockYinItem) => {
+    handleFadeIn(blockYinItem, resultBtn);
+  });
+});
+
+blockYinTop.forEach((blockYinTopItem) => {
+  handleFadeIn(blockYinTopItem, aboutBtn);
+});
+window.addEventListener("scroll", () => {
+  blockYinTop.forEach((blockYinTopItem) => {
+    handleFadeIn(blockYinTopItem, aboutBtn);
+  });
+});
+
+// Відкладене завантаження відео для ортимізації
+
+document.querySelectorAll(".lazy-video").forEach((video) => {
+  video.addEventListener("click", () => {
+    const videoId = video.dataset.videoId;
+    const iframe = document.createElement("iframe");
+    iframe.setAttribute(
+      "src",
+      `https://www.youtube.com/embed/${videoId}?autoplay=1`
+    );
+    iframe.setAttribute("allowfullscreen", "");
+    iframe.className = "video_placeholder video_iframe";
+    iframe.setAttribute(
+      "allow",
+      "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    );
+    video.innerHTML = "";
+    video.appendChild(iframe);
+  });
+});
+
+// Відкриття меню на фокусі
+
+const btnCourse = document.getElementById("btn_course");
+
+function handleKeydown(event) {
+  if (event.key === "Enter" || event.key === " ") {
+    event.preventDefault();
+    btnCourse.click();
+    btnCourse.removeEventListener("keydown", handleKeydown);
+  }
+}
+
+btnCourse.addEventListener("keydown", handleKeydown);
