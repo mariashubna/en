523f0b522a17cfe3f86bbc55374b26507e9b433f
Mariia <mariashubna@gmail.com>
2025-10-26 17:16:57 +0100

    main_page

--- js/main.js
+++ js/main.js
@@ -253,3 +253,19 @@ function handleKeydown(event) {
 }
 
 btnCourse.addEventListener("keydown", handleKeydown);
+
+// Плавне переміщення за посиланням по сторінці
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute("href"));
+    if (target) {
+      target.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+    this.blur();
+  });
+});
